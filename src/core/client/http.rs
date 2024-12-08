3ea081671bb569fa66e173e3e6f4930b0606f7a9
nehu3n <developer.nh3@gmail.com>
2024-12-07 23:26:48 -0300

    🔨 refactor(client): Added anyhow for error handling.

--- src/core/client/http.rs
+++ src/core/client/http.rs
@@ -1,9 +1,10 @@
+use anyhow::{Context, Result};
 use reqwest::Client;
 
 use super::response::{NPMPackage, Package, Registry};
 
 impl Package {
-    pub async fn get_package(self) -> Result<NPMPackage, String> {
+    pub async fn get_package(self) -> Result<NPMPackage> {
         let name = if !self.author.is_empty() {
             format!("{}/{}", self.author, self.name)
         } else {
@@ -15,8 +16,15 @@ impl Package {
                 let registry_url = format!("https://registry.npmjs.org/{}/{}", name, self.version);
 
                 let client = Client::new();
-                let response = client.get(&registry_url).send().await.unwrap();
-                let package = response.json::<NPMPackage>().await.unwrap();
+                let response = client
+                    .get(&registry_url)
+                    .send()
+                    .await
+                    .context("Error fetching package from registry")?;
+                let package = response
+                    .json::<NPMPackage>()
+                    .await
+                    .context("Error parsing package")?;
 
                 Ok(package)
             }
@@ -27,9 +35,13 @@ impl Package {
     }
 }
 
-pub async fn get_tarball(url: String) -> Result<Vec<u8>, String> {
-    let response = reqwest::get(url).await.unwrap();
-    let tarball = response.bytes().await.unwrap().to_vec();
+pub async fn get_tarball(url: String) -> Result<Vec<u8>> {
+    let response = reqwest::get(url).await.context("Error fetching tarball")?;
+    let tarball = response
+        .bytes()
+        .await
+        .context("Error streaming tarball")?
+        .to_vec();
 
     Ok(tarball)
 }
